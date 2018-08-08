8e98e29add54a50d70e9aad66c6ae20f728bf969
QuanPV <quanpv.hut@gmial.com>
2018-08-08 17:49:53 +0700

    [QuanPV] add product cell

---
+++ LoginApp/Views/TableViewCells/ProductTableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  ProductTableViewCell.swift
+//  LoginApp
+//
+//  Created by Quan Pham Van on 8/8/18.
+//  Copyright © 2018 QuanPV. All rights reserved.
+//
+
+import UIKit
+
+protocol ProductTableViewCellDelegate: class {
+    func didTapBuy(forProduct product: Product)
+}
+
+
+class ProductTableViewCell: UITableViewCell {
+
+    @IBOutlet var priceButton: UIButton!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var productImageView: UIImageView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    weak var delegate: ProductTableViewCellDelegate?
+    
+    var productCellViewModel: ProductCellViewModel! {
+        didSet {
+            self.nameLabel.text = productCellViewModel.name
+            self.priceButton.setTitle(productCellViewModel.displayPrice, for: .normal)
+            self.descriptionLabel.text = productCellViewModel.desc
+            self.productImageView.downloaded(from: productCellViewModel.imageURL)
+        }
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        self.priceButton.layer.borderColor = Color.omiseGOBlue.cgColor()
+        self.priceButton.layer.borderWidth = 1
+    }
+
+    @IBAction func didTapBuyButton(_ sender: UIButton) {
+        print("didTapBuyButton:====>")
+        self.delegate?.didTapBuy(forProduct: self.productCellViewModel.product)
+    }
+}
